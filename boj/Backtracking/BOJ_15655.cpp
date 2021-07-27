031fb180885c2e833107d34db706bae03a096783
skyqnaqna <skyqnaqna@naver.com>
2021-07-27 21:05:29 +0900

    Add BOJ 15655, 15656, 15657 files

---
+++ boj/Backtracking/BOJ_15655.cpp
@@ -0,0 +1,54 @@
+/*
+ 백준 15655 N과 M (6)
+ 21.07.27
+ https://github.com/skyqnaqna/algorithm_study
+ */
+
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+using dd = double;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+
+#define INF 1e9
+#define endl "\n"
+
+int n, m;
+vector <int> v, res;
+
+void backtracking(int idx, int cnt) {
+    if (cnt == m) {
+        for (auto &i : res) {
+            cout << i << " ";
+        }
+        cout << endl;
+        return;
+    }
+
+    for (int i = idx; i < n; ++i) {
+        res.push_back(v[i]);
+        backtracking(i + 1, cnt + 1);
+        res.pop_back();
+    }
+}
+
+int main()
+{
+    ios::sync_with_stdio(0); cin.tie(0);
+    freopen("input.txt", "r", stdin);
+
+    cin >> n >> m;
+
+    for (int i = 0; i < n; ++i) {
+        int x; cin >> x;
+        v.push_back(x);
+    }
+
+    sort(v.begin(), v.end());
+
+    backtracking(0, 0);
+
+    return 0;
+}
