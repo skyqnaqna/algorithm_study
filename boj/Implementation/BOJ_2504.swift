a056792efd649a833269dd497af04977a5f5f054
skyqnaqna <skyqnaqna@naver.com>
2021-08-18 17:04:20 +0900

    Modify BOJ 2504 file

--- boj/Implementation/BOJ_2504.swift
+++ boj/Implementation/BOJ_2504.swift
@@ -1,56 +1,83 @@
-// 백준 2504 괄호의 값
-// 21.04.02
+/*
+ 백준 2504 괄호의 값
+ 21.08.18
+ https://github.com/skyqnaqna/algorithm_study
+ */
 
 import Foundation
 
-let str = Array(readLine()!.map { String($0) })
+let input = readLine()!
+let bracket = ["(", ")", "[", "]"]
+var stack = [String]()
 
-var st = [String]()
-var flag = false
+func addNums(_ n: Int) {
+  var num = n
 
-for s in str {
-  if s == "(" || s == "[" {
-    st.append(s)
-  } else if s == ")" {
-    var x = 2
-    
-    if !st.isEmpty && st.last != "(" && st.last != "[" {
-      x *= Int(st.removeLast())!
-    }
-    
-    if st.last == "[" || st.isEmpty {
-      flag = true
+  while !stack.isEmpty && !bracket.contains(stack.last!) {
+    num += Int(stack.removeLast())!
+  }
+
+  stack.append(String(num))
+}
+
+func multiNums(_ n: Int, _ pair: String) -> Bool {
+  var num = n
+
+  while !stack.isEmpty && !bracket.contains(stack.last!) {
+    num *= Int(stack.removeLast())!
+  }
+
+  if stack.isEmpty || stack.last! != pair {
+    return false
+  } else {
+    stack.removeLast()
+  }
+
+  if !stack.isEmpty && !bracket.contains(stack.last!) {
+    num += Int(stack.removeLast())!
+  }
+
+  stack.append(String(num))
+  return true
+}
+
+for now in input {
+  if now == "(" || now == "[" {
+    stack.append(String(now))
+  } else {
+    if stack.isEmpty {
       break
-    } else if st.last == "(" {
-      st.removeLast()
-      
-      if st.last != "(" && st.last != "[" && !st.isEmpty {
-        x += Int(st.removeLast())!
+    }
+
+    if now == ")" {
+      if stack.last! == "(" {
+        stack.removeLast()
+        addNums(2)
+      } else if stack.last! != "[" {
+        if !multiNums(2, "(") {
+          break
+        }
+      } else {
+        break
       }
-      
-      st.append(String(x))
-    }
-  } else if s == "]" {
-    var x = 3
-    
-    if !st.isEmpty && st.last != "(" && st.last != "[" {
-      x *= Int(st.removeLast())!
-    }
-    
-    if st.last == "(" || st.isEmpty {
-      flag = true
-      break
-    } else if st.last == "[" {
-      st.removeLast()
-      
-      if st.last != "(" && st.last != "[" && !st.isEmpty {
-        x += Int(st.removeLast())!
+
+    } else if now == "]" {
+      if stack.last! == "[" {
+        stack.removeLast()
+        addNums(3)
+      } else if stack.last! != "(" {
+        if !multiNums(3, "[") {
+          break
+        }
+      } else {
+        break
       }
     }
-    
-    st.append(String(x))
   }
 }
 
-if flag || st.count > 1 { print(0) }
-else { print(Int(st.last!)!) }
+if stack.isEmpty || stack.count > 1 || bracket.contains(stack[0]) {
+  print(0)
+} else {
+  print(stack[0])
+}
