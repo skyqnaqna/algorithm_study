8dbfee3a265ae3706786b03bb13536a43ed7a328
skyqnaqna <skyqnaqna@naver.com>
2021-08-18 19:40:24 +0900

    Modify BOJ 2021 file

--- boj/Dijkstra/BOJ_2021.cpp
+++ boj/Dijkstra/BOJ_2021.cpp
@@ -15,20 +15,16 @@ using pll = pair<ll, ll>;
 #define INF 2e9
 #define endl "\n"
 
-const int MAX = 100001;
-
-int n, m, from, to, distances[MAX]; // 역간 거리
-vector <vector <int> > stations, lines;
-bool visited[MAX]; // 노선 방문 체크
+int n, m, from, to;
 
 int main() {
     ios::sync_with_stdio(0); cin.tie(0);
     freopen("input.txt", "r", stdin);
 
     cin >> n >> m;
-    stations.resize(n + 1, vector<int>());
-    lines.resize(m, vector<int>());
-    fill(distances, distances + MAX, INF);
+    vector <vector <int> > stations(n + 1), lines(m);
+    vector <int> station_distance(n + 1, -1); // 각 역까지의 환승 횟수
+    vector <bool> visitedLine(m, false); // 각 노선 방문 체크
 
     for (int i = 0; i < m; ++i) {
         int x; cin >> x;
@@ -41,41 +37,40 @@ int main() {
 
     cin >> from >> to;
 
-    priority_queue<pii>pq;
+    queue<pii>q;
 
     // 시작 역과 해당 노선에 있는 역들의 거리 초기화
     for (auto &line : stations[from]) {
-        visited[line] = true;
+        visitedLine[line] = true;
         for (auto &station : lines[line]) {
-            distances[station] = 0;
-            pq.push({0, station});
+            if (station_distance[station] == -1) {
+                station_distance[station] = 0;
+                q.push({0, station});
+            }
         }
     }
 
-    while (!pq.empty()) {
-        int dist = -pq.top().first;
-        int now = pq.top().second;
-        pq.pop();
-
-        if (dist > distances[now]) continue;
+    while (!q.empty()) {
+        int dist = q.front().first;
+        int now = q.front().second;
+        q.pop();
 
         for (auto &line : stations[now]) {
-            if (!visited[line]) {
-                visited[line] = true;
+            if (!visitedLine[line]) {
+                visitedLine[line] = true;
 
                 // 방문하지 않은 노선이라면 해당 노선의 역들의 거리를 갱신해준다
                 for (auto &station : lines[line]) {
-                    if (dist + 1 < distances[station]) {
-                        distances[station] = dist + 1;
-                        pq.push({-(dist + 1), station});
+                    if (station_distance[station] == -1) {
+                        station_distance[station] = dist + 1;
+                        q.push({dist + 1, station});
                     }
                 }
             }
         }
     }
 
-    if (distances[to] < INF) cout << distances[to] << endl;
-    else cout << -1 << endl;
+    cout << station_distance[to] << endl;
 
     return 0;
 }
