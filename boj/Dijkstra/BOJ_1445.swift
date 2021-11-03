90b8dfb40e11c589d1fafe94d7b1b7ba356a3d25
skyqnaqna <skyqnaqna@naver.com>
2021-11-04 00:02:58 +0900

    Modify BOJ 1445 file

--- boj/Dijkstra/BOJ_1445.swift
+++ boj/Dijkstra/BOJ_1445.swift
@@ -98,12 +98,15 @@ let direction = [(-1, 0), (1, 0), (0, -1), (0, 1)]
 
 var start = (0, 0), end = (0, 0)
 var graph = [[Int]](repeating: [Int](repeating: 0, count: m), count: n)
+
+// (쓰레기 통과, 쓰레기 인접)
 var dist = [[(Int, Int)]](repeating: [(Int, Int)](repeating: (INF, INF), count: m), count: n)
 
 func inBound(_ r: Int, _ c: Int) -> Bool {
   return 0 <= r && r < n && 0 <= c && c < m
 }
 
+// 주변에 쓰레기가 있으면 true
 func checkAround(_ r: Int, _ c: Int) -> Bool {
   for (dy, dx) in direction {
     let nr = r + dy
@@ -161,21 +164,19 @@ while !pq.isEmpty {
       continue
     }
     
+    // 다음 지점이 쓰레기가 있는지 없는지에 따라
+    // 통과한 쓰레기가 더 적은 곳이거나 쓰레기 옆을 지나온 칸의 개수가 더 적으면 Push
     if graph[nr][nc] == -1 {
-      if pathCnt + 1 < dist[nr][nc].0 {
-        pq.push((pathCnt + 1, sideCnt, (nr, nc)))
-        dist[nr][nc] = (pathCnt + 1, sideCnt)
-      } else if pathCnt + 1 == dist[nr][nc].0 && sideCnt < dist[nr][nc].1 {
+      if pathCnt + 1 < dist[nr][nc].0 ||
+          (pathCnt + 1 == dist[nr][nc].0 && sideCnt < dist[nr][nc].1) {
         pq.push((pathCnt + 1, sideCnt, (nr, nc)))
         dist[nr][nc] = (pathCnt + 1, sideCnt)
       }
     } else {
       let trash = checkAround(nr, nc) ? 1 : 0
       
-      if pathCnt < dist[nr][nc].0 {
-        pq.push((pathCnt, sideCnt + trash, (nr, nc)))
-        dist[nr][nc] = (pathCnt, sideCnt + trash)
-      } else if pathCnt == dist[nr][nc].0 && sideCnt + trash < dist[nr][nc].1 {
+      if pathCnt < dist[nr][nc].0 ||
+          (pathCnt == dist[nr][nc].0 && sideCnt + trash < dist[nr][nc].1) {
         pq.push((pathCnt, sideCnt + trash, (nr, nc)))
         dist[nr][nc] = (pathCnt, sideCnt + trash)
       }
