8db43100753431352b28f9500a7eda0df1aed0c8
skyqnaqna <skyqnaqna@naver.com>
2022-02-23 17:05:31 +0900

    Modify BOJ 2417 files

--- boj/BinarySearch/BOJ_2417.cpp
+++ boj/BinarySearch/BOJ_2417.cpp
@@ -1,6 +1,6 @@
 /*
  백준 2417 정수 제곱근
- 21.07.31
+ 22.02.23
  https://github.com/skyqnaqna/algorithm_study
  */
 
@@ -8,6 +8,7 @@
 
 using namespace std;
 using ll = long long;
+using ull = unsigned long long;
 using dd = double;
 using pii = pair<int, int>;
 using pll = pair<ll, ll>;
@@ -15,29 +16,34 @@ using pll = pair<ll, ll>;
 #define INF 2e9
 #define endl "\n"
 
-ll n;
+ull n;
 
-int main()
-{
-    ios::sync_with_stdio(0); cin.tie(0);
-    freopen("input.txt", "r", stdin);
-
-    cin >> n;
-
-    ll mid, l = 0, r = n, ans = n;
-
-    while (l <= r) {
-        mid = (l + r) / 2;
-
-        if (mid >= sqrt(n)) {
-            ans = min(ans, mid);
-            r = mid - 1;
-        } else {
-            l = mid + 1;
-        }
-    }
-
-    cout << ans << endl;
-
+int main() {
+  ios::sync_with_stdio(0); cin.tie(0);
+  
+  cin >> n;
+  
+  if (n == 0) {
+    cout << 0 << endl;
     return 0;
+  }
+  
+  ull mid, l = 0, r = pow(2, 63), ans = 0;
+  
+  
+  while (l <= r) {
+    mid = (l + r) / 2;
+    
+    if (pow(mid, 2) >= n) {
+      ans = mid;
+      r = mid - 1;
+    } else {
+      l = mid + 1;
+    }
+  }
+  
+  cout << ans << endl;
+  
+  return 0;
 }
+
--- boj/BinarySearch/BOJ_2417.swift
+++ boj/BinarySearch/BOJ_2417.swift
@@ -1,25 +1,28 @@
 /*
  백준 2417 정수 제곱근
- 21.08.23
+ 22.02.23
  https://github.com/skyqnaqna/algorithm_study
  */
 
 import Foundation
 
-let n = Int(readLine()!)!
+let n = UInt64(readLine()!)!
 
-var l = 0, r = n, ans = n
-
-let sqrtN = sqrt(Double(n))
-while l <= r {
-  let mid = (l + r) / 2
-
-  if Double(mid) >= sqrtN {
-    ans = min(ans, mid)
-    r = mid - 1
-  } else {
-    l = mid + 1
+if n == 0 { print(0) }
+else {
+  var l: UInt64 = 0, r = UInt64(sqrt(Double(UInt64.max))) + 1, ans: UInt64 = 0
+  
+  while l <= r {
+    let mid = (l + r) / 2
+    
+    if n <= mid * mid {
+      ans = mid
+      r = mid - 1
+    } else {
+      l = mid + 1
+    }
   }
+  
+  print(ans)
 }
 
-print(ans)
